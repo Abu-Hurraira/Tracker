0a55683845042c4f4a02dda905be87f386488bff
hp <hp@HURRAIRA>
2026-07-04 11:35:32 +0500

    Azure Deployment

--- backend/TrackerAPI/obj/Debug/net9.0/TrackerAPI.AssemblyInfo.cs
+++ backend/TrackerAPI/obj/Debug/net9.0/TrackerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d8be3e6a7508a669a64c7cf77201bfe3f486cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed839d7e48fb2520e7fcff70be1b120c6598283b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
