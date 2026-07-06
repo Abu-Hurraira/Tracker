851dd7fd6ef2261ca01d8d8b230af913ad032183
hp <hp@HURRAIRA>
2026-07-06 10:59:42 +0500

    updated

--- backend/TrackerAPI/obj/Debug/net9.0/TrackerAPI.AssemblyInfo.cs
+++ backend/TrackerAPI/obj/Debug/net9.0/TrackerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839f36bb516c61a6f7b57ae6ae8d32562f695b74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7240996a7e0604650a0c47ba145a4c4ff314b047")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
